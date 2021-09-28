d3c15662ea716a3e3f9e10426be8e327516c7e0c
renata.andrade.lima <renata.andrade.lima@avanade.com>
2021-09-27 23:22:30 -0300

    feat: created component footer

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Footer = () => {
+  return (
+    <footer className="navbar bg-primary fixed-bottom">
+      <div className="container-fluid align-items-center justify-content-center">
+        <span className="text-white">oBoticário</span>
+      </div>
+    </footer>
+  )
+}
