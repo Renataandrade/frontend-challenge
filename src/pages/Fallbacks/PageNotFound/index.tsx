b4f32fbffb71be90d1b8c1fc331b698baa92b332
renata.andrade.lima <renata.andrade.lima@avanade.com>
2021-09-28 10:39:11 -0300

    feat: created fallback pages

---
+++ src/pages/Fallbacks/PageNotFound/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const PageNotFound = () => {
+  return (
+    <div className="min-vh-100 d-flex justify-content-center align-items-center">
+      <div className="flex-column">
+        <div className="col-12">
+          <h4>Página não encontrada :(</h4>
+        </div>
+      </div>
+    </div>
+  )
+}
