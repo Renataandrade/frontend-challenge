4f8bbb637b5f249bc1c7aa082583baf9a1c336a7
renata.andrade.lima <renata.andrade.lima@avanade.com>
2021-10-01 09:55:27 -0300

    refactor: changed em_andamento to em_validacao

--- src/enums/status.ts
+++ src/enums/status.ts
@@ -1,11 +1,11 @@
 import * as IStatus from "../models/Purchase"
 
 export const APROVADO: IStatus.Aprovado = 'APROVADO'
-export const EM_ANDAMENTO: IStatus.Andamento = 'EM_ANDAMENTO'
+export const EM_VALIDACAO: IStatus.Andamento = 'EM_VALIDACAO'
 export const REPROVADO: IStatus.Reprovado = 'REPROVADO'
 
 export const STATUS = Object.freeze({
   [APROVADO]: 'Aprovado',
-  [EM_ANDAMENTO]: 'Em validação',
+  [EM_VALIDACAO]: 'Em validação',
   [REPROVADO]: 'Reprovado'
 })
